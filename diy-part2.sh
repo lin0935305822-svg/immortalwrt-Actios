e5972f28ea341f540b5f16d3d8b0bdbe371c1823
Developer <dev@meter-weight.local>
2026-07-28 12:03:54 +0800

    fix(ufi003): make BlueZ config deterministic

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,15 +28,3 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
 # git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
 
-# 蓝牙 SPP / UART HCI / USB Fallback / BlueZ / RFCOMM 强制使能
-cat << 'EOF' >> .config
-CONFIG_PACKAGE_kmod-bluetooth=y
-CONFIG_PACKAGE_kmod-btqcomsmd=y
-CONFIG_PACKAGE_kmod-hci-uart=y
-CONFIG_PACKAGE_kmod-btusb=y
-CONFIG_PACKAGE_bluez-daemon=y
-CONFIG_PACKAGE_bluez-utils=y
-CONFIG_PACKAGE_bluez-libs=y
-CONFIG_PACKAGE_dbus=y
-EOF
-
